8809ca9207a59ac6fca9ac169e75ee43119f018b
linus mutugi mate <mlynux@gmail.com>
2022-08-16 14:09:18 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+const num1 = 12.4;
+const num2 = 5;
+const multiply = num1*num2;
+multiply;
+
+const random = Math.ceil(Math.random());
+
+const num3 = 25;
+const num4 = 7;
+const mod = num3 % num4;
+
+
+const max = Math.max(1, 2, 3, 4, 5, 20);
